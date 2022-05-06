7ae28a0dcf6da6f16bc3f672a38e5da74276404b
maii <eng.maii.emad@gmail.com>
2022-05-06 18:44:19 +0200

    Adding Basic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesDetailsComponent } from './moviesDetails/moviesDetails.component';
+import { TvComponent } from './tv/tv.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [						
+    AppComponent,
+      HomeComponent,
+      LoginComponent,
+      RegisterComponent,
+      NavbarComponent,
+      AboutComponent,
+      MoviesComponent,
+      MoviesDetailsComponent,
+      TvComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/moviesDetails/moviesDetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moviesDetails',
+  templateUrl: './moviesDetails.component.html',
+  styleUrls: ['./moviesDetails.component.scss']
+})
+export class MoviesDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
